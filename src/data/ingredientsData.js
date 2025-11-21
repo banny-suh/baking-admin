6a46446c5cf6e6d859e5162a5addc2d43f65e96b
banny-suh <banny.suh@gmail.com>
2025-11-22 02:36:20 +0900

    feat: 재료 목록 요소 높이 조절

--- src/data/ingredientsData.js
+++ src/data/ingredientsData.js
@@ -1,8 +1,8 @@
 export const ingredientsData = [
     { "id": "levain001", "name": "천연발효종 스타터", "gramsPurchased": 100, "price": 300 },
     { "id": "flour001", "name": "오가그레인 우리밀 맷돌제분 강력분", "gramsPurchased": 10000, "price": 40000 },
-    { "id": "flour002", "name": "마루비시 케이 블레소레이유", "gramsPurchased": 20000, "price": 35500 },
-    { "id": "flour003", "name": "곰표 강력 다목적용", "gramsPurchased": 5000, "price": 8950 },
+    { "id": "flour002", "name": "마루비시 케이 블레소레이유 강력분", "gramsPurchased": 20000, "price": 35500 },
+    { "id": "flour003", "name": "곰표 다목적용 강력분", "gramsPurchased": 5000, "price": 8950 },
     { "id": "flour004", "name": "밥스브레드밀 통밀", "gramsPurchased": 2270, "price": 14900 },
     { "id": "fats001", "name": "엑스트라버진 올리브유", "gramsPurchased": 1000, "price": 25990 },
     { "id": "dairy001", "name": "앵커 버터 (냉동)", "gramsPurchased": 454, "price": 8000 },
